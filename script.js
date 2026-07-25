889db64ef2d8025b40bd702e72e4b17aff14de3a
Deepika Arepalli <arepallideepika5@gmail.com>
2026-07-25 23:32:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("h1").style.transition = "color 2s";
+    document.querySelector("h1").style.color = "#e74c3c";
+});
